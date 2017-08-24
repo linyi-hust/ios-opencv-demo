909e65728c0fdfc591804c3adf0aa22ee31d7016
林一 <linyi@linyis-Mac.local>
2017-08-24 19:35:10 +0800

    Initial Commit

---
+++ OpenCV_Demo/VideoViewController.mm
@@ -0,0 +1,410 @@
+//
+//  VideoViewController.m
+//  OpenCV_Demo
+//
+//  Created by 张昭 on 25/11/2016.
+//  Copyright © 2016 张昭. All rights reserved.
+//
+
+#import "VideoViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import "OpenCVUtil.h"
+#import <opencv2/opencv.hpp>
+#import <opencv2/imgproc/types_c.h>
+#import <opencv2/imgcodecs/ios.h>
+
+typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
+
+@interface VideoViewController () <AVCapturePhotoCaptureDelegate, AVCaptureVideoDataOutputSampleBufferDelegate>
+@property (strong, nonatomic) IBOutlet UIImageView *containerImageView;
+
+@property (strong, nonatomic) IBOutlet UIImageView *resultImageView;
+@property (strong, nonatomic) CAShapeLayer *shapeLayer;
+
+@property (strong, nonatomic) AVCaptureSession *captureSession;
+@property (strong, nonatomic) AVCaptureDeviceInput *captureDeviceInput;
+@property (strong, nonatomic) AVCapturePhotoOutput *capturePhotoOutput;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+
+
+@end
+
+@implementation VideoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    _shapeLayer = [CAShapeLayer layer];
+    _shapeLayer.frame = _resultImageView.bounds;
+    [_resultImageView.layer addSublayer:_shapeLayer];
+    _shapeLayer.lineWidth = 1;
+    _shapeLayer.strokeColor = [UIColor redColor].CGColor;
+    _shapeLayer.fillColor = [UIColor clearColor].CGColor;
+    _resultImageView.contentMode = UIViewContentModeScaleToFill;
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self.captureSession startRunning];
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    [self.captureSession stopRunning];
+}
+
+-(void)dealloc{
+    [self removeNotification];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    _captureSession = [[AVCaptureSession alloc] init];
+    if ([_captureSession canSetSessionPreset:AVCaptureSessionPresetMedium]) {
+        _captureSession.sessionPreset = AVCaptureSessionPresetMedium;
+    }
+    
+    // 获取输入设备
+    AVCaptureDevice *captureDevice = [self getCameraDeviceWithPosition:AVCaptureDevicePositionFront];
+    if (!captureDevice) {
+        NSLog(@"摄像头获取出现错误");
+        return;
+    }
+    
+    // 初始化设备数据输入对象
+    NSError *error = nil;
+    _captureDeviceInput = [[AVCaptureDeviceInput alloc] initWithDevice:captureDevice error:&error];
+    if (error) {
+        NSLog(@"设备输入对象获取失败：%@", error.localizedDescription);
+        return;
+    }
+    
+    if ([_captureSession canAddInput:_captureDeviceInput]) {
+        [_captureSession addInput:_captureDeviceInput];
+    }
+    
+    AVCaptureVideoDataOutput *captureVideoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
+    captureVideoDataOutput.alwaysDiscardsLateVideoFrames = YES;
+    dispatch_queue_t queue = dispatch_queue_create("cameraQueue", NULL);
+    [captureVideoDataOutput setSampleBufferDelegate:self queue:queue];
+    NSString* key = (NSString*)kCVPixelBufferPixelFormatTypeKey;
+    NSNumber* value = [NSNumber numberWithUnsignedInt:kCVPixelFormatType_32BGRA];
+    NSDictionary* videoSettings = [NSDictionary
+                                   dictionaryWithObject:value forKey:key];
+    [captureVideoDataOutput setVideoSettings:videoSettings];
+    if ([_captureSession canAddOutput:captureVideoDataOutput]) {
+        [_captureSession addOutput:captureVideoDataOutput];
+    }
+    
+    // 视图预览，实时展示摄像头
+    _captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_captureSession];
+    
+    CALayer *layer = _containerImageView.layer;
+    layer.masksToBounds = YES;
+    _captureVideoPreviewLayer.frame = layer.bounds;
+    _captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    [layer insertSublayer:_captureVideoPreviewLayer below:_shapeLayer];
+
+    [self addNotificationToCaptureDevice:captureDevice];
+ 
+    
+}
+
+
+
+
+
+// 切换摄像头
+- (IBAction)clickedChangeCaptureBtn:(id)sender {
+    AVCaptureDevice *currentDevice=[self.captureDeviceInput device];
+    AVCaptureDevicePosition currentPosition=[currentDevice position];
+    [self removeNotificationFromCaptureDevice:currentDevice];
+    AVCaptureDevice *toChangeDevice;
+    AVCaptureDevicePosition toChangePosition=AVCaptureDevicePositionFront;
+    if (currentPosition==AVCaptureDevicePositionUnspecified||currentPosition==AVCaptureDevicePositionFront) {
+        toChangePosition=AVCaptureDevicePositionBack;
+    }
+    toChangeDevice=[self getCameraDeviceWithPosition:toChangePosition];
+    [self addNotificationToCaptureDevice:toChangeDevice];
+    //获得要调整的设备输入对象
+    AVCaptureDeviceInput *toChangeDeviceInput=[[AVCaptureDeviceInput alloc]initWithDevice:toChangeDevice error:nil];
+    
+    //改变会话的配置前一定要先开启配置，配置完成后提交配置改变
+    [self.captureSession beginConfiguration];
+    //移除原有输入对象
+    [self.captureSession removeInput:self.captureDeviceInput];
+    //添加新的输入对象
+    if ([self.captureSession canAddInput:toChangeDeviceInput]) {
+        [self.captureSession addInput:toChangeDeviceInput];
+        self.captureDeviceInput=toChangeDeviceInput;
+    }
+    //提交会话配置
+    [self.captureSession commitConfiguration];
+}
+
+
+
+
+
+// 获取指定位置的摄像头
+- (AVCaptureDevice *)getCameraDeviceWithPosition:(AVCaptureDevicePosition)position {
+    AVCaptureDeviceDiscoverySession *discoverySession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeBuiltInWideAngleCamera] mediaType:AVMediaTypeVideo position:position];
+    for (AVCaptureDevice *camera in discoverySession.devices) {
+        if ([camera position]==position) {
+            return camera;
+        }
+    }
+    return nil;
+}
+
+/**
+*  设备连接成功
+*
+*  @param notification 通知对象
+*/
+-(void)deviceConnected:(NSNotification *)notification{
+    NSLog(@"设备已连接...");
+}
+/**
+ *  设备连接断开
+ *
+ *  @param notification 通知对象
+ */
+-(void)deviceDisconnected:(NSNotification *)notification{
+    NSLog(@"设备已断开.");
+}
+/**
+ *  捕获区域改变
+ *
+ *  @param notification 通知对象
+ */
+-(void)areaChange:(NSNotification *)notification{
+    NSLog(@"捕获区域改变...");
+}
+
+/**
+ *  会话出错
+ *
+ *  @param notification 通知对象
+ */
+-(void)sessionRuntimeError:(NSNotification *)notification{
+    NSLog(@"会话发生错误.");
+}
+
+-(void)changeDeviceProperty:(PropertyChangeBlock)propertyChange{
+    AVCaptureDevice *captureDevice= [self.captureDeviceInput device];
+    NSError *error;
+    //注意改变设备属性前一定要首先调用lockForConfiguration:调用完之后使用unlockForConfiguration方法解锁
+    if ([captureDevice lockForConfiguration:&error]) {
+        propertyChange(captureDevice);
+        [captureDevice unlockForConfiguration];
+    }else{
+        NSLog(@"设置设备属性过程发生错误，错误信息：%@",error.localizedDescription);
+    }
+}
+
+
+#pragma mark - 通知
+/**
+ *  给输入设备添加通知
+ */
+-(void)addNotificationToCaptureDevice:(AVCaptureDevice *)captureDevice{
+    //注意添加区域改变捕获通知必须首先设置设备允许捕获
+    [self changeDeviceProperty:^(AVCaptureDevice *captureDevice) {
+        captureDevice.subjectAreaChangeMonitoringEnabled=YES;
+    }];
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    //捕获区域发生改变
+    [notificationCenter addObserver:self selector:@selector(areaChange:) name:AVCaptureDeviceSubjectAreaDidChangeNotification object:captureDevice];
+}
+-(void)removeNotificationFromCaptureDevice:(AVCaptureDevice *)captureDevice{
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    [notificationCenter removeObserver:self name:AVCaptureDeviceSubjectAreaDidChangeNotification object:captureDevice];
+}
+/**
+ *  移除所有通知
+ */
+-(void)removeNotification{
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    [notificationCenter removeObserver:self];
+}
+
+-(void)addNotificationToCaptureSession:(AVCaptureSession *)captureSession{
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    //会话出错
+    [notificationCenter addObserver:self selector:@selector(sessionRuntimeError:) name:AVCaptureSessionRuntimeErrorNotification object:captureSession];
+}
+
+#pragma mark AVCaptureSession delegate
+- (void)captureOutput:(AVCaptureOutput *)captureOutput
+didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
+       fromConnection:(AVCaptureConnection *)connection
+{
+    static int proccessing = 0;
+    if (proccessing) {
+        return;
+    }
+    proccessing = 1;
+    
+    
+    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CVPixelBufferLockBaseAddress(imageBuffer,0);
+    uint8_t *baseAddress = (uint8_t *)CVPixelBufferGetBaseAddress(imageBuffer);
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
+    size_t width = CVPixelBufferGetWidth(imageBuffer);
+    size_t height = CVPixelBufferGetHeight(imageBuffer);
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef newContext = CGBitmapContextCreate(baseAddress,
+                                                    width, height, 8, bytesPerRow, colorSpace,
+                                                    kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+    CGImageRef newImage = CGBitmapContextCreateImage(newContext);
+    
+    CGContextRelease(newContext);
+    CGColorSpaceRelease(colorSpace);
+    
+    
+    UIImage *image= [UIImage imageWithCGImage:newImage scale:0.5 orientation:UIImageOrientationLeftMirrored];
+    
+    CGImageRelease(newImage);
+    //        self.resultImageView.image = image;
+    
+    CVPixelBufferUnlockBaseAddress(imageBuffer,0);
+    //    __weak __typeof(self) weakSelf = self;
+    //    dispatch_async(dispatch_get_main_queue(), ^{
+    //        weakSelf.resultImage.image = image;
+    //    });
+    
+    
+    NSArray *rectArray = [OpenCVUtil facePointDetectForImage:[self fixOrientation:image]];
+    
+    
+    if (rectArray.count > 0) {
+        UIBezierPath* totalPath = [UIBezierPath bezierPath];
+        for (NSNumber* rectValue in rectArray) {
+            CGRect rect = [rectValue CGRectValue];
+            rect = [self convertRectFromRect:rect toSize:_resultImageView.bounds.size];
+            UIBezierPath *subpath = [UIBezierPath bezierPathWithRect:rect];
+            [totalPath appendPath:subpath];
+        }
+        
+        __weak __typeof(self) weakSelf = self;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            weakSelf.shapeLayer.path = totalPath.CGPath;
+            weakSelf.resultImageView.image = image;
+            proccessing = 0;
+        });
+    }else{
+        proccessing = 0;
+    }
+    
+    //    __weak __typeof(self) weakSelf = self;
+    //    dispatch_async(dispatch_get_main_queue(), ^{
+    //        weakSelf.resultImage.image = [OpenCVUtil faceDetectForImage:[self fixOrientation:image]];
+    //        proccessing = 0;
+    //    });
+}
+
+- (UIImage *)fixOrientation:(UIImage *)aImage {
+    
+    // No-op if the orientation is already correct
+    if (aImage.imageOrientation == UIImageOrientationUp)
+        return aImage;
+    
+    // We need to calculate the proper transformation to make the image upright.
+    // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    
+    switch (aImage.imageOrientation) {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, aImage.size.width, aImage.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, aImage.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+            
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, aImage.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        default:
+            break;
+    }
+    
+    switch (aImage.imageOrientation) {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, aImage.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+            
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, aImage.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        default:
+            break;
+    }
+    
+    // Now we draw the underlying CGImage into a new context, applying the transform
+    // calculated above.
+    CGContextRef ctx = CGBitmapContextCreate(NULL, aImage.size.width, aImage.size.height,
+                                             CGImageGetBitsPerComponent(aImage.CGImage), 0,
+                                             CGImageGetColorSpace(aImage.CGImage),
+                                             CGImageGetBitmapInfo(aImage.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    switch (aImage.imageOrientation) {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            // Grr...
+            CGContextDrawImage(ctx, CGRectMake(0,0,aImage.size.height,aImage.size.width), aImage.CGImage);
+            break;
+            
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,aImage.size.width,aImage.size.height), aImage.CGImage);
+            break;
+    }
+    
+    // And now we just create a new UIImage from the drawing context
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    return img;
+}
+
+- (CGRect)convertRectFromRect:(CGRect)fromRect toSize:(CGSize)size{
+    
+    return CGRectMake(size.width*fromRect.origin.x, size.height*fromRect.origin.y,size.width*fromRect.size.width, size.height*fromRect.size.height);
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ #pragma mark - Navigation
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
